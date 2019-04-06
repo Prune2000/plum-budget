5fe260499119e9616d8c6a4be2a5538843f6455e
Prune2000 <camille@plommetre.com>
2019-04-06 17:06:31 +0200

    Add current month in the header

--- client/src/models/Header/Header.js
+++ client/src/models/Header/Header.js
@@ -7,12 +7,18 @@ class Header extends React.Component {
     constructor(props) {
       super(props);
     }
+
     render() {
+
+      let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+      let date = new Date();
+      let month = months[date.getMonth() - 1];
+
       return (
         <div className="top">
             <div className="budget">
                 <div className="budget__title">
-                    Available Budget in <span className="budget__title--month">%Month%</span>:
+                    Available Budget in <span className="budget__title--month">{month}</span>:
                 </div>
                 
                 <div className="budget__value">+ 2,345.64</div>
