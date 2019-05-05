06af43a2322dde8f124192c3b55f9c15f1ee4c54
Prune2000 <pruniercamille@gmail.com>
2019-05-05 14:35:47 +0200

    Style the home page

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -7,17 +7,29 @@ class Welcome extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="top">
-          <h1>Welcome to Plum Budget!</h1>
+        <div className="top__welcome">
+          <div className="top__menu">
+            <ul>
+              <li>
+                <a href="/signup">Sign-up</a>
+              </li>
+              <li>
+                <a href="/login">Login</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="top__logo">
+            <img src={require('./plum2.png')} />  
+            <p>Plum Budget is a secure and confidential budget app. No tracking, no 3rd parties, no ads. Create an account and start tracking your spending now!</p>
+          </div>
+
         </div>
-        <ul>
-          <li>
-            <a href="/signup">Sign-up</a>
-          </li>
-          <li>
-            <a href="/login">Login</a>
-          </li>
-        </ul>
+        <div>
+          <p>Plum Budget is a secure and confidential budget app. Too many budget apps will sell financial information to 3rd parties as well as track users to get better insights on their shopping habits. This app aims to give a free and confidential app to track your daily finances.
+
+          Plum budget is an open-source project, so feel free to help! Contact me on twitter or by email if you need help!</p>
+        </div>
       </div>
       
     );
