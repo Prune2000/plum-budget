441d643ceab9123c75e1ec67d24e750b20ea2dc5
Prune2000 <pruniercamille@gmail.com>
2019-05-12 18:55:54 +0200

    Remove the scrolling bar from the side menu

--- client/src/components/MenuSide/MenuSide.js
+++ client/src/components/MenuSide/MenuSide.js
@@ -5,37 +5,15 @@ import YearlyBudget from '../YearlyBudget/YearlyBudget';
 import UserInfo from '../UserInfo/UserInfo';
 
 class MenuSide extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          username: this.props.username,
-        }
-    }
-
-  showSettings (event) {
-    event.preventDefault();
-  }
 
   render () {
     return (
       <Menu>
         <UserInfo />
         <YearlyBudget />
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
       </Menu>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-
-    console.log(state.user.username);
-
-    return {
-      username: state.user.username
-    };
-  }
-
-export default connect(mapStateToProps)(MenuSide);
+export default connect()(MenuSide);
