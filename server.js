0c6cd3f9dd6d87c0b133caf9ac64cd12b54504ee
Prune2000 <pruniercamille@gmail.com>
2019-05-19 13:56:38 +0200

    Add server handling for heroku

--- server.js
+++ server.js
@@ -43,6 +43,16 @@ require('./controllers/auth/authController')(app, passport);
 require('./controllers/api/apiController')(app, passport);
 //apiController(app, passport);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
